664b55ea9e679e8c2cbca8af04615c076befc788
Jonathan Slenders <jonathan@slenders.be>
2016-06-10 20:16:52 +0200

    Fix in styling. (prompt_toolkit change.)

--- pypager/style.py
+++ pypager/style.py
@@ -2,6 +2,7 @@ from __future__ import unicode_literals
 from prompt_toolkit.styles.from_pygments import style_from_pygments
 from prompt_toolkit.token import Token
 from prompt_toolkit.styles import Attrs, Style
+from prompt_toolkit.styles.utils import split_token_in_parts, merge_attrs
 
 __all__ = (
     'create_style',
@@ -48,6 +49,12 @@ class PypagerStyle(Style):
         self.ui_style = style_from_pygments(style_dict=ui_style)
 
     def get_attrs_for_token(self, token):
+        result = []
+        for part in split_token_in_parts(token):
+            result.append(self._get_attrs_for_token(part))
+        return merge_attrs(result)
+
+    def _get_attrs_for_token(self, token):
         if token and token[0] == 'C':
             # Token starts with ('C',). Token describes its own style.
             c, fg, bg, bold, underline, italic, blink, reverse = token
